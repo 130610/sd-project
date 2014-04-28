9c41c1077ef90bbef455ac1bd3275e813e3583bb
Bryan Ames <b130610@gmail.com>
2014-04-27 20:00:38 -0500

    parseTargets now finds target lines (simple search) and prints their line number

--- source/target.cpp
+++ source/target.cpp
@@ -1,16 +1,59 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <regex>
 #include "target.h"
 using namespace std;
 
+void Target::addChildren(string dl)
+{
+	Target **tmpList;
+	SplitString depends(dl);
+	vector<string> dependsList = depends.split(' ');
+	tmpList = new Target*[numChildren + dependsList.size()];
+
+	for (int i = 0; i < numChildren; i++) {
+		tmpList[i] = children[i];
+	}
+	for (int i = 0; i < dependsList.size(); i++) {
+		tmpList[i + numChildren] = new Target(dependsList[i]);
+	}
+
+	delete [] children;
+	children = tmpList;
+}
+
+vector<string>& SplitString::split(char delim)
+{
+	if (!flds.empty()) flds.clear();
+	string work = data();
+	string buf = "";
+	int i = 0;
+	while (i < work.length()) {
+		if (work[i] != delim) {
+			buf += work[i];
+		} else if (buf.length() > 0) {
+			flds.push_back(buf);
+			buf = "";
+		}
+		i++;
+	}
+	if (!buf.empty()) {
+		flds.push_back(buf);
+	}
+	return flds;
+}
+
 Target *parseTargets(const char *filename)
 {
+	Target *root;
 	string *lineList = new string[1]; 
 	char line[128];
 	char c;
 	int i = 0;
+	int numTargets = 0;
 	ifstream ifs(filename);
+	string tName, tDepends;
 
 	while (ifs.get(c)) {
 		line[i++] = c;
@@ -22,8 +65,16 @@ Target *parseTargets(const char *filename)
 	}
 
 	for (int i = 0; i < getLineListSize(lineList); i++) {
-		
+		if (matchTargetLine(lineList[i], tName, tDepends) && numTargets == 0) {
+			root = new Target(tName);
+			root->addChildren(tDepends);
+			numTargets++;
+			cout << "made root at " << i << endl;
+		} else if (matchTargetLine(lineList[i], tName, tDepends)) {
+			cout << "found target at " << i << endl;
+		}
 	}
+	return root;
 }
 
 void addLine(string *&ll, char *l)
@@ -36,8 +87,7 @@ void addLine(string *&ll, char *l)
 	for (int i = 0; i < len; i++) {
 		tmpList[i] = ll[i];
 	}
-	tmpList[len - 1] = l;
-	//cout << l;
+	if (l[0]) tmpList[len - 1] = l;
 	tmpList[len] = "";
 
 	delete [] ll;
@@ -50,3 +100,30 @@ int getLineListSize(string *ll)
 	while (ll[len++] != "");
 	return len;
 }
+
+// reads a string s, and returns whether it is the declaration of a rule in a
+// makefile; reads the rule into the string n, and the dependencies into d
+bool matchTargetLine(string l, string& n, string& d)
+{
+	int i = 0;
+	bool inName = true;
+	n = d = "";
+
+	if (l[0] == '\t' || l[0] == '\n') {
+		return false;
+	}
+
+	while (l[i] != '\n' && i < l.length()) {
+		if (l[i] == ':'){
+			inName = false;
+			while (l[++i] == ' ');
+		} else if (inName) {
+			n.append(1, l[i]);
+		} else if (!inName) {
+			d.append(1, l[i]);
+		}
+		i++;
+	}
+	return true;
+}
+
--- source/target.h
+++ source/target.h
@@ -2,6 +2,7 @@
 #define _TARGET_
 
 #include <string>
+#include <vector>
 using namespace std;
 
 class Target
@@ -11,14 +12,27 @@ class Target
 	Target **parents;
 	int posX;
 	int posY;
+	int numParents;
+	int numChildren;
 
 public:
-	Target(): targetName(""), children(0), parents(0), posX(0), posY(0) {}
-	Target(const char *n): targetName(n), children(0), parents(0), posX(0), posY(0) {}
+	Target(): targetName(""), children(0), parents(0), posX(0), posY(0), numParents(0), numChildren(0) {}
+	Target(string n): targetName(n), children(0), parents(0), posX(0), posY(0), numParents(0), numChildren(0) {}
+
+	void addChildren(string dl);
+	void setName(string n) { targetName = n; }
+};
+
+class SplitString : public string {
+    vector<string> flds;
+public:
+    SplitString(string s) : string(s) { };
+    vector<string>& split(char delim);
 };
 
 Target *parseTargets(const char *filename);
 void addLine(string *&ll, char *l);
 int getLineListSize(string *ll);
+bool matchTargetLine(string l, string& n, string& d);
 
 #endif
