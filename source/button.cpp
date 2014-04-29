91ee440f3b5e3d47aabb606833f90d042afc5386
Soren I. Bjornstad <soren.bjornstad@gmail.com>
2014-04-29 12:03:30 -0500

    standardize formatting in button.cpp

--- source/button.cpp
+++ source/button.cpp
@@ -2,18 +2,19 @@
 #include "text.h"
 
 void Button::draw() {
+  // coordinates
   int x0 = min(x, x + width);
   int x1 = max(x, x + width);
   int y0 = min(y, y + height);
   int y1 = max(y, y + height);
 
-  // button
 
- //buttonpress
-  if( IsPressed ) glColor3f(1., 1., 1.);//turns white;
-  else if ( overButton ) glColor3f(0., 1., 0.);
-  else glColor3f(color.red, color.green, color.blue);
-  
+  // color selection, based on pressed status
+  if( IsPressed ) glColor3f(1., 1., 1.); // white;
+  else if ( overButton ) glColor3f(0., 1., 0.); // green
+  else glColor3f(color.red, color.green, color.blue); // as defined in object
+
+  // button
   glBegin(GL_QUADS);
     glVertex2f(x0, y0);
     glVertex2f(x1, y0);
@@ -21,7 +22,7 @@ void Button::draw() {
     glVertex2f(x0, y1);
   glEnd();
 
-  //border
+  // border
   glColor3f(0,0,0);
   glBegin(GL_LINE_STRIP);
     glVertex2f(x0, y0);
@@ -37,17 +38,13 @@ void Button::draw() {
   bText[label.size()] = '\0';
   drawBitmapText(bText,labelStartX, y + (height / 2) - 4,0);
   delete[] bText;
-
- 
 }
 
 bool Button::onButton(int xmousepos, int ymousepos)
-  {
-    
-    int newymousepos = 768-ymousepos; /*mouse position and graphics 
-					position have different point 
-					of origin*/
-    return xmousepos>= x && xmousepos<=(x+width) && newymousepos >=y &&
-      newymousepos <= (y+height);
-    
-  }
+{
+  int newymousepos = 768-ymousepos; /* mouse position and graphics 
+                                       position have different point 
+                                       of origin */
+  return xmousepos>= x && xmousepos<=(x+width) && newymousepos >=y &&
+         newymousepos <= (y+height);
+}
