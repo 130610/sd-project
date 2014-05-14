db8323ba5371a36b61bce64a499dae2634e9b0d0
Soren Bjornstad <soren.bjornstad@gmail.com>
2014-05-14 17:19:36 -0500

    some preparation for packaging

--- source/start.cpp
+++ source/start.cpp
@@ -1,6 +1,7 @@
-#define DEBUG // allows quitting with 'q' to avoid the quit sequence
-//#define MOUSECOORDS // display current mouse posn in terminal
-#define INFINITEJUMPS // what it sounds like
+/* debugging options */
+//#define DEBUG         // allows quitting with 'q' to avoid the quit sequence
+//#define MOUSECOORDS   // display current mouse posn in terminal
+//#define INFINITEJUMPS // what it sounds like
 
 #include <iostream>
 #include <iomanip>
